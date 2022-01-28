fb63436d73caf61b6af70213c3403b3a8cc24781
polizoto <polizoto@hotmail.com>
2022-01-28 01:06:35 -0500

    Connect front end to back end

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
+// const fs = require('fs');
+const path = require('path');
+
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const apiRoutes = require('./routes/apiRoutes');
+
 const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json())
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
   });
